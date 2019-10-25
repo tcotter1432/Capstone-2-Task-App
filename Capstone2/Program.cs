e3da6a41dfd021f374b6ad943c62b96b2240168d
tcotter1432 <tcotter1432@gmail.com>
2019-10-25 15:36:05 -0400

    Capstone 2 First Push

---
+++ Capstone2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone2
+{
+    class Program
+    {
+        //static List<Task> theTasks = new List<Task>();
+        static void Main(string[] args)
+        {
+            //string input;
+            //int intInput;
+
+            while (true)
+            {
+                Console.WriteLine("What do you want to do?");
+                PrintOptions();
+                SelectMenuOption(Console.ReadLine());
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static int ValidateInput(string thingToValidate)
+        {
+            try
+            {
+                return int.Parse(thingToValidate);
+            }catch (FormatException)
+            {
+                Console.WriteLine("Bad input, try again...");
+                return ValidateInput(Console.ReadLine());
+            }
+        }
+
+        public static void PrintOptions()
+        {
+            Console.WriteLine("1. List tasks");
+            Console.WriteLine("2. Add task");
+            Console.WriteLine("3. Delete task");
+            Console.WriteLine("4. Mark task complete");
+            Console.WriteLine("5. Quit");
+        }
+
+        public static void AddTask()
+        {
+            Task task = new Task();
+            task.Name = GetUserInput("Enter task owner's Name: ");
+            task.Description = GetUserInput("What is this Task? ");
+            task.DueDate = CheckDateTime(GetUserInput("When is it due? "));
+            task.Completed = false;
+            Task.AddTask(task);
+        }
+
+        public static DateTime CheckDateTime(string input)
+        {
+            try
+            {
+                return DateTime.Parse(input);
+            }
+            catch (FormatException)
+            {
+                return CheckDateTime(GetUserInput("You entered a bad."));
+            }
+        }
+
+        public static void SelectMenuOption(string userInput)
+        {
+            switch (userInput.ToLower())
+            {
+                case ("1"):
+                case ("list tasks"):
+                    Task.DisplayTasks();
+                    break;
+                case ("2"):
+                case ("add task"):
+                    AddTask();
+                    break;
+                case ("3"):
+                case ("delete task"):
+                    Task.DisplayTasks();
+                    Console.WriteLine("Enter the number you want to delete.");
+                    Task.DeleteTask(ValidateInput(Console.ReadLine())-1);
+                    break;
+                case ("4"):
+                case ("mark task complete"):
+                    Task.DisplayTasks();
+                    Console.WriteLine("Which task is now complete?");
+                    Task.CompleteTask(ValidateInput(Console.ReadLine()) - 1);
+                    break;
+                case ("5"):
+                case ("quit"):
+                    Environment.Exit(0);
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Capstone2/Task.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone2
+{
+    class Task
+    {
+        static List<Task> theTasks = new List<Task> {
+            new Task("Jack","Does the work",DateTime.Parse("10/25/2019")),
+            new Task("Tristan","Stares Blankly",DateTime.Parse("10/24/2019")),
+            new Task("Steve","Does Nothing",DateTime.Parse("10/25/2019"))
+        };
+
+        private string name;
+        private string description;
+        private DateTime dueDate;
+        private bool completed;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public DateTime DueDate
+        {
+            get { return dueDate; }
+            set { dueDate = value; }
+        }
+        public bool Completed
+        {
+            get { return completed; }
+            set { completed = value; }
+        }
+
+        public Task()
+        {
+
+        }
+        public Task(string _name, string _description, DateTime _dueDate, bool _complete = false)
+        {
+            Name = _name;
+            Description = _description;
+            DueDate = _dueDate;
+            Completed = _complete;
+        }
+
+        public static void DisplayTasks()
+        {
+            for (int i = 0; i < theTasks.Count; i++)
+            {
+                DisplayTask(i);
+            }
+        }
+        public static void DisplayTask(int i)
+        {
+            Console.WriteLine($"\t{i + 1}. {theTasks[i].name}");
+            Console.WriteLine($"\tDue Date: {theTasks[i].DueDate.ToShortDateString()}");
+            Console.WriteLine($"\tCompleted? {theTasks[i].Completed}");
+            Console.WriteLine($"\tTask: {theTasks[i].Description}\n");
+        }
+
+        public static void AddTask(Task task)
+        {
+            theTasks.Add(task);
+        }
+
+        public static void DeleteTask(int index)
+        {
+            if (index > 0 && index < theTasks.Count)
+            {
+                DisplayTask(index);
+                Console.WriteLine("Do you want to eliminate this Task? (y/n)");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    theTasks.RemoveAt(index);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bad input. Try again, dummy.");
+                DisplayTasks();
+                try
+                {
+                    DeleteTask(int.Parse(Console.ReadLine()));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Bad Input. Try again.");
+                    DeleteTask(int.Parse(Console.ReadLine()));
+                }
+            }
+        }
+
+        private void Complete()
+        {
+            completed = true;
+        }
+
+        public static void CompleteTask(int index)
+        {
+            if (index > 0 && index < theTasks.Count)
+            {
+                DisplayTask(index);
+                Console.WriteLine("Do you want to complete this Task? (y/n)");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    theTasks[index].Complete();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bad input. Try again, dummy.");
+                DisplayTasks();
+                try
+                {
+                    DeleteTask(int.Parse(Console.ReadLine()));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Bad Input. Try again.");
+                    DeleteTask(int.Parse(Console.ReadLine()));
+                }
+            }
+        }
+    }
+}
